7fc14bdd6b7b10c2df389b78d34ee00eb95e1bcc
coder.flower <coder.flower@gmail.com>
2019-03-06 21:35:19 +0800

    初始化项目

---
+++ LTY/LTY/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LTY
+//
+//  Created by 花菜 on 2019/3/6.
+//  Copyright © 2019 Coder.flower. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
